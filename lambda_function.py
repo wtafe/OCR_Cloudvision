b50a53031e5833bec5e32b3cf1506016999303d7
wtafe <35377312+wtafe@users.noreply.github.com>
2018-04-26 22:09:28 -0400

    OCR_Cloudvision

---
+++ lambda_function.py
@@ -0,0 +1,47 @@
+import boto3
+import json
+from botocore.vendored import requests
+import base64
+
+GOOGLE_CLOUD_VISION_API_URL = 'https://vision.googleapis.com/v1/images:annotate?key='
+s3 = boto3.resource('s3')
+
+def goog_cloud_vision (event, context):
+    image =get_object_body(event['bucket'], event['key'])
+    encoded_img = base64.b64encode(image).decode('utf-8') 
+    TxtKey = "OCR-"+event['key']+".txt"
+    api_url = GOOGLE_CLOUD_VISION_API_URL + "AIzaSyAnFQngSxRc3Rrj_LlHG4eCI2D58EEdhM8"
+    req_body = json.dumps({
+        'requests': [{
+            'image': {
+                'content': encoded_img
+            },
+            'features': [{
+                'type': 'TEXT_DETECTION',
+                'maxResults': 10,
+            }]
+        }]
+    })
+    res = requests.post(api_url, data=req_body)
+
+    text = (res.json()['responses'][0]['fullTextAnnotation']['text'])
+	#text1 = (res.json()["responses"][0]["textAnnotations"][0].get("description", None))
+    text = text.replace('\r', '').replace('\n', '')
+    print (text)
+    s3.Bucket("ist440grp2ocr").put_object(Key=TxtKey, Body=text, ACL='public-read', ContentType='txt/html')
+    output = {
+        "bucket": "ist440grp2ocr",
+        "key": TxtKey
+    }
+    return output
+def get_object_body(bucket, key):
+    """
+    Get object body in S3
+    :param bucket: bucket name
+    :param key: object key
+    :return: binary strings
+    """
+    obj = s3.Object(bucket, key)
+    response = obj.get()
+    body = response['Body'].read()
+    return body
